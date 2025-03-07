bcbd3719c81c3e3935841f472034ee806bfa8d41
Breno2307 <breno.ferreira.candido@gmail.com>
2025-03-06 21:54:47 -0300

    adicionando arquivos da aula03

---
+++ aulas/aula03/App.js
@@ -0,0 +1,26 @@
+import { View } from "react-native";
+
+function App() {
+  return (
+    <View style={{ flex: 1, flexDirection: "row", backgroundColor: "white" }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-around",
+          alignItems: "center",
+          backgroundColor: "red",
+        }}
+        >
+        </View>
+      <View style={{ height: 50, width: 50, backgroundColor: "black" }}
+      ></View>
+      <View style={{ height: 50, width: 50, backgroundColor: "blue" }}
+      ></View>
+      <View style={{ height: 50, width: 50, backgroundColor: "yellow" }}
+      ></View>
+      <View style={{ flex: 1, backgroundColor: "green" }}></View>
+   </View>
+  );
+}
+
+export default App;
--- praticas/pratica02/App.js
+++ praticas/pratica02/App.js
@@ -1,30 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
+
 function AppBar(props) {
-  <View
-    style={{
-      height: 50,
-      backgroundColor: "#333",
-      justifyContent: "center",
-      paddingLeft: 16,
-    }}
-  >
-    <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}></Text>
+  <View style={styles.appBar}>
+    <Text style={styles.title}></Text>
   </View>;
 }
 function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <AppBar titulo="Minha aplicação React Native" />
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "#fff",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      ></View>
+      <View></View>
     </View>
   );
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appBar: {
+    height: 50,
+    backgroundColor: "#333",
+    justifyContent: "center",
+    paddingLeft: 16,
+  },
+  title: { fontSize: 20, fontWeight: "bold", color: "#fff" },
+});
+
 export default App;
