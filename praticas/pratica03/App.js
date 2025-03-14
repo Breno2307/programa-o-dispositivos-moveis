9a4cad2cd0c587d1ae0d3d95c56922c37906a99d
Breno2307 <breno.ferreira.candido@gmail.com>
2025-03-13 21:48:46 -0300

    adicionando pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,10 @@
+import GalleryScreen from "./screens/GalleryScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ListScreen from "./screens/ListScreen";
+import ProfileScreen from "./screens/ProfleScreen";
+
+function App() {
+  return <ProfileScreen />;
+}
+
+export default App;
