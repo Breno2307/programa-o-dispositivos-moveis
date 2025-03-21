86f859ab16912d1e497f620b587219a2218a18fb
Breno2307 <breno.ferreira.candido@gmail.com>
2025-03-20 21:47:14 -0300

    adicionando continuacao aula04

--- aulas/aula04/App.js
+++ aulas/aula04/App.js
@@ -1,8 +1,10 @@
+import Conversas from "./screens/Conversas";
+import DetalhesProduto from "./screens/DetalhesProduto";
 import Galeria from "./screens/Galeria";
 import RegistrarConta from "./screens/RegistrarConta";
 
-function App(){
-return <Galeria />
+function App() {
+  return <Conversas />
 }
 
 export default App;
