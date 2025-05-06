a57c3de80c680ef71d6cb5d05c6d548fdd1dd2e9
unknown <2424290034@ACADEMICO.IESB.BR>
2025-05-05 21:51:08 -0300

    adfiodhaovgi

---
+++ aulas/aula08/screens/RedefinirSenha.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { TextInput, Button, HelperText } from "react-native-paper";
+
+function RedefinirSenha() {
+  const [email, setEmail] = useState("");
+  const [erro, setErro] = useState("");
+
+
+  const trataValor = (text) => {
+    setEmail(text);
+  }
+ 
+  const trataEnviar = () => {
+    if (!email) {
+      setErro("E-mail é obrigatorio");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      setErro("E-mail inválido");
+      return;
+    }
+
+    setEmail("");
+    setErro("");
+    alert("E-mail enviado!");
+  };
+
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <TextInput
+        autoCapitalize="none"
+        keyboardType="email-address"
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        mode="outlined"
+        style={{ marginBottom: 16 }}
+        error={erro!=""}
+      />
+      <HelperText type="error" visible={erro}>
+        {erro}
+      </HelperText>
+      <Button mode="contained" onPress={trataEnviar}>
+        Enviar
+      </Button>
+    </View>
+  );
+}
+
+export default RedefinirSenha;
